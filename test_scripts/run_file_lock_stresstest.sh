d9e93de58fc42083df2bd6c20b8fba1b2abd403f
m <m@bitsnbites.eu>
2021-07-09 10:54:08 +0200

    run_file_lock_stresstest: Change tab-width to 4

--- test_scripts/run_file_lock_stresstest.sh
+++ test_scripts/run_file_lock_stresstest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# -*- mode: sh; tab-width: 2; indent-tabs-mode: nil; -*-
+# -*- mode: sh; tab-width: 4; indent-tabs-mode: nil; -*-
 # ------------------------------------------------------------------------------
 # Copyright (c) 2020 Marcus Geelnard
 #
@@ -27,53 +27,53 @@
 total_success=true
 
 function run_test {
-  LOCALLOCKS=$1
-  TESTFILE=/tmp/bc_file_lock_stresstest_data-$$
+    LOCALLOCKS=$1
+    TESTFILE=/tmp/bc_file_lock_stresstest_data-$$
 
-  rm -f "$TESTFILE"
+    rm -f "$TESTFILE"
 
-  test_type="network share safe locks"
-  if [[ "$1" = "true" ]] ; then
-    test_type="allow local locks"
-  fi
-
-  # Run four instances of the stresstest in parallel.
-  echo "Starting four concurrent processes (${test_type})..."
-  pids=""
-  for _ in {1..4}; do
-    base/file_lock_stresstest "$TESTFILE" "$LOCALLOCKS" &
-    pids+=" $!"
-  done
-
-  # Wait for all processes to finish.
-  got_error=false
-  for p in $pids ; do
-    if ! wait "$p" ; then
-      got_error=true
+    test_type="network share safe locks"
+    if [[ "$1" = "true" ]] ; then
+        test_type="allow local locks"
     fi
-  done
 
-  # Get the result and delete the file.
-  DATA=$(cat "$TESTFILE")
-  rm -f "$TESTFILE"
+    # Run four instances of the stresstest in parallel.
+    echo "Starting four concurrent processes (${test_type})..."
+    pids=""
+    for _ in {1..4}; do
+        base/file_lock_stresstest "$TESTFILE" "$LOCALLOCKS" &
+        pids+=" $!"
+    done
 
-  # Delete the lock file (if any).
-  rm -f "${TESTFILE}.lock"
+    # Wait for all processes to finish.
+    got_error=false
+    for p in $pids ; do
+        if ! wait "$p" ; then
+            got_error=true
+        fi
+    done
 
-  # Did we have an error exit status from any of the processes?
-  if $got_error ; then
-    echo "*** FAIL: At least one of the processes failed."
-    exit 1
-  fi
+    # Get the result and delete the file.
+    DATA=$(cat "$TESTFILE")
+    rm -f "$TESTFILE"
 
-  # Check the data file contents.
-  EXPECTED_DATA="4000"
-  if [[ "${DATA}" = "${EXPECTED_DATA}" ]] ; then
-    echo "The test passed!"
-  else
-    echo "*** FAIL: The count should be ${EXPECTED_DATA}, but is ${DATA}."
-    total_success=false
-  fi
+    # Delete the lock file (if any).
+    rm -f "${TESTFILE}.lock"
+
+    # Did we have an error exit status from any of the processes?
+    if $got_error ; then
+        echo "*** FAIL: At least one of the processes failed."
+        exit 1
+    fi
+
+    # Check the data file contents.
+    EXPECTED_DATA="4000"
+    if [[ "${DATA}" = "${EXPECTED_DATA}" ]] ; then
+        echo "The test passed!"
+    else
+        echo "*** FAIL: The count should be ${EXPECTED_DATA}, but is ${DATA}."
+        total_success=false
+    fi
 }
 
 # Without local locks.
@@ -84,8 +84,8 @@ run_test true
 
 # Check the test result.
 if $total_success ; then
-  echo "All tests passed!"
-  exit 0
+    echo "All tests passed!"
+    exit 0
 fi
 
 echo "*** FAIL: At least one of the tests failed."
